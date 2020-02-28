ca18510b991ef1705d77a6df14915ef6e454d6e1
Corey Cook <cookc7438@rwps.org>
2020-02-28 16:47:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   recipes.key='value'
   Object.assign(recipes, {})
 }
+function updateObjectWithKeyAndValue(object, key, value) {
+  Object.assign(recipes, {})
+}
