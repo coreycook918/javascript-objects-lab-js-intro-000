c507ae6c92ba69095a6d068b46707b0c301a594f
Corey Cook <cookc7438@rwps.org>
2020-02-28 16:59:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function deleteFromObjectByKey(object, key) {
   delete clone[key];
   return clone;
 }
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
